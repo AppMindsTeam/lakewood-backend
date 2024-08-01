c6246d8738e337bf3a8f387ef98a4441b10e3396
Muhammad Hassan <hassan0501651@gmail.com>
2024-08-01 12:04:37 +0500

    deep linking

--- functions/index.js
+++ functions/index.js
@@ -19,46 +19,52 @@ admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
 
-function getUser(userId) {
-  // [START get_document]
-  let userRef = db.collection("Users").doc(userId);
-  let getDoc = userRef
-    .get()
-    .then((doc) => {
-      if (!doc.exists) {
-        console.log("No such document!");
-        return 0;
-      } else {
-        console.log("Document data:", doc.data());
-        return doc.data();
-      }
-    })
-    .catch((err) => {
-      console.log("Error getting document", err);
-    });
-  // [END get_document]
-  return getDoc;
-}
+// function getUser(userId) {
+//   // [START get_document]
+//   let userRef = db.collection("Users").doc(userId);
+//   let getDoc = userRef
+//     .get()
+//     .then((doc) => {
+//       if (!doc.exists) {
+//         console.log("No such document!");
+//         return 0;
+//       } else {
+//         console.log("Document data:", doc.data());
+//         return doc.data();
+//       }
+//     })
+//     .catch((err) => {
+//       console.log("Error getting document", err);
+//     });
+//   // [END get_document]
+//   return getDoc;
+// }
 
 exports.sendNotification = functions.firestore
   .document("Notifications/{id}")
   .onCreate(async (snapshot, context) => {
-    // const payload = {
-    //   notification: {
-    //     title: `${snapshot.data().title}`,
-    //     body: `${snapshot.data().body}`,
-    //     icon: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-    //     sound: "",
-    //   },
-    // };
+    let recipientDetails;
+    if (snapshot.data().recipientId) {
+      recipientDetails = {
+        recipientId: snapshot.data().recipientId,
+        productId: snapshot.data().productId,
+        screen: "ChatDetails",
+      };
+    } else {
+      recipientDetails = {};
+    }
+
+    const dataPayload = JSON.stringify(recipientDetails);
+
     const message = {
       // tokens: [snapshot.data().receiverToken],
       tokens: snapshot.data().receiverToken,
       notification: {
         title: `${snapshot.data().title}`,
         body: `${snapshot.data().body}`,
-        // icon: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-        // sound: "",
+      },
+      data: {
+        details: dataPayload, // Wrap the stringified payload in an object
       },
       apns: {
         headers: {
