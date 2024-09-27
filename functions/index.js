7633bfc8359013a10673feeb5762575856111800
Ali Waqar <aliwaqar981@gmail.com>
2024-09-28 00:15:02 +0500

    added method to sync Products on typesense

--- functions/index.js
+++ functions/index.js
@@ -9,15 +9,52 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const { onRequest } = require('firebase-functions/v2/https');
+const logger = require('firebase-functions/logger');
 
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
+
+const Typesense = require('typesense');
+
+const client = new Typesense.Client({
+  nodes: [
+    {
+      host: 'n2zuwmjsilropd1xp-1.a1.typesense.net', // Replace with your Typesense server host
+      port: 443, // Replace with your server port
+      protocol: 'https', // Use 'https' if using Typesense Cloud or a secured server
+    },
+  ],
+  apiKey: '7J6yMPeyWPSbPbR4q9tNtp3cgwi1fbc8', // Replace with your API key
+  connectionTimeoutSeconds: 4,
+});
+
+// Firestore trigger for new product addition
+exports.syncNewProductToTypesense = functions.firestore
+  .document('Products/{productId}')
+  .onCreate(async (snap, context) => {
+    const newProduct = snap.data();
+    const productId = context.params.productId;
+
+    try {
+      // Add the new product to Typesense
+      await client
+        .collections('Products')
+        .documents()
+        .create({
+          ...newProduct,
+          id: productId,
+          createdAt: newProduct.createdAt ? newProduct.createdAt.toDate() : '',
+        });
+      logger.log(`Successfully added product ${productId} to Typesense`);
+    } catch (error) {
+      logger.error(`Error adding product ${productId} to Typesense:`, error);
+    }
+  });
 
 // function getUser(userId) {
 //   // [START get_document]
@@ -41,14 +78,14 @@ db.settings({ timestampsInSnapshots: true });
 // }
 
 exports.sendNotification = functions.firestore
-  .document("Notifications/{id}")
+  .document('Notifications/{id}')
   .onCreate(async (snapshot, context) => {
     let recipientDetails;
     if (snapshot.data().recipientId) {
       recipientDetails = {
         recipientId: snapshot.data().recipientId,
         productId: snapshot.data().productId,
-        screen: "ChatDetails",
+        screen: 'ChatDetails',
       };
     } else {
       recipientDetails = {};
@@ -68,22 +105,22 @@ exports.sendNotification = functions.firestore
       },
       apns: {
         headers: {
-          "apns-priority": "10",
+          'apns-priority': '10',
         },
         payload: {
           aps: {
-            sound: "horn.wav",
+            sound: 'horn.wav',
           },
         },
       },
       ios: {
-        sound: "horn.wav",
+        sound: 'horn.wav',
       },
       android: {
-        priority: "high",
+        priority: 'high',
         notification: {
-          channel_id: "sound_channel",
-          sound: "horn.wav",
+          channel_id: 'sound_channel',
+          sound: 'horn.wav',
         },
       },
     };
@@ -94,7 +131,7 @@ exports.sendNotification = functions.firestore
       .then((response) => {
         return logger.info(`A new notification for  =====`, response);
       })
-      .catch((e) => logger.info("------ error:", e));
+      .catch((e) => logger.info('------ error:', e));
   });
 
 /* eslint-enable */
